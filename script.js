78d35c2626864054863c50b9b1264506d81d7770
Sergey Smolin <smsergey@outlook.com>
2016-05-20 15:28:47 +0300

    Add presenter and separate text coordinates calculation

--- script.js
+++ script.js
@@ -6,36 +6,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function initSVG () {
     svgElement = document.createElementNS(nsString, "svg");
-    svgElement.setAttribute('width', 400);
-    svgElement.setAttribute('height', 400);
+    svgElement.setAttribute('width', 200);
+    svgElement.setAttribute('height', 200);
     document.getElementsByTagName("body")[0].appendChild(svgElement);
   }
 
   function NodeFactory () {
 
     var create = {
-      rectangle: function () {
+      rectangle: function (width, height) {
         var element = createElement('rect');
         element.setAttribute("fill", "#ff0000");
-        element.setAttribute("width", "200px");
-        element.setAttribute("height", "200px");
+        element.setAttribute("width", presenter.checkWidth(width, 'rectangle'));
+        element.setAttribute("height", presenter.checkHeight(height, 'rectangle'));
         return element;
       },
       group: function () {
         var element = createElement('g');
         return element;
       },
-      text: function (text) {
+      text: function (text, coords) {
         var element = createElement('text');
-        element.setAttribute('x', '10');
-        element.setAttribute('y', '10');
-        element.textContent = text;  
+        element.textContent = text;
         return element;
       },
       textRectangle: function (text) {
-        var rectangle = create.rectangle();
+        var rectangle = create.rectangle(width, height);
         var group = create.group();
-        var text = create.text(text);
+        var text = create.text();
+          element.setAttribute('x', '10');
+          element.setAttribute('y', '10');
         group.appendChild(rectangle);
         group.appendChild(text);
         return group;
@@ -57,8 +57,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
   }
 
+  var defaults = {
+    rectangle: {
+      width: 200,
+      height: 200
+    }
+  };
+
+  function Presenter () {
+    return {
+      checkWidth: function (width, element) {
+        var width = width ? width : defaults[element].width;
+        return width + 'px';
+      },
+      checkHeight: function (height, element) {
+        var height = height ? height : defaults[element].height;
+        return height + 'px';
+      }
+    };
+  }
+
   initSVG();
-  var factory = new NodeFactory ();
+  var factory = new NodeFactory();
+  var presenter = new Presetner();
   factory.appendNode(factory.createNode('textRectangle', 'Hello World!'));
 
 });
